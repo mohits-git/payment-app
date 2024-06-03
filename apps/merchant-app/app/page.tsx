a53a496c14b0336e5374ffa0181716dec5d0d4cf
mohits-git <mohitkhatri2805@gmail.com>
2024-06-03 20:52:10 +0530

    refac: rename web and doc to user-app and merchant-app and removed unncessary starter code

---
+++ apps/merchant-app/app/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+type Props = {
+
+}
+
+const Page: React.FC<Props> = () => {
+    return (
+        <>
+            <h1>Payment App</h1>
+            <h2>Merchant App</h2>
+        </>
+    )
+}
+
+export default Page
