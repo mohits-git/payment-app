ede663ab50102fd5e41a4c3a69fe11fa1e1d3304
mohits-git <mohitkhatri2805@gmail.com>
2024-06-04 18:56:03 +0530

    refac(user-app): change providers.tsx file position to root

--- apps/user-app/providers.tsx
+++ apps/user-app/providers.tsx
@@ -2,11 +2,14 @@
 
 import { ReactNode } from "react";
 import { RecoilRoot } from "recoil";
+import { SessionProvider } from "next-auth/react";
 
 export const Providers = ({ children }: { children: ReactNode }) => {
   return (
     <RecoilRoot>
-      {children}
+      <SessionProvider>
+        {children}
+      </SessionProvider>
     </RecoilRoot>
   )
 }
