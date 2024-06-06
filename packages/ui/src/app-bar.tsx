ed3632624f743eb5265fbf94bf76d13352b62457
mohits-git <mohitkhatri2805@gmail.com>
2024-06-06 17:12:46 +0530

    refac(ui/Appbar): refac Appbar styling/ui

--- packages/ui/src/app-bar.tsx
+++ packages/ui/src/app-bar.tsx
@@ -1,25 +1,29 @@
 import { Button } from "./button";
 
 interface AppbarProps {
-    user?: {
-        name?: string | null;
-    },
-    // TODO: figure out type 
-    onSignin: any,
-    onSignout: any
+  user?: {
+    name?: string | null;
+  },
+  onSignin: any,
+  onSignout: any
 }
 
 export const Appbar = ({
-    user,
-    onSignin,
-    onSignout
+  user,
+  onSignin,
+  onSignout
 }: AppbarProps) => {
-    return <div className="flex justify-between border-b px-4">
-        <div className="text-lg flex flex-col justify-center">
-            PayTM
-        </div>
-        <div className="flex flex-col justify-center pt-2">
-            <Button onClick={user ? onSignout : onSignin}>{user ? "Logout" : "Login"}</Button>
-        </div>
+  return <div className="flex justify-between border-b px-4">
+    <div className="text-xl font-semibold text-[#6a51a6] flex flex-col justify-center">
+      PayTM
     </div>
+    <div className="flex flex-col justify-center pt-2">
+      <Button
+      className="bg-[#6a51a6] hover:bg-[#6d57aa]"
+        onClick={user ? onSignout : onSignin}
+      >
+        {user ? "Logout" : "Login"}
+      </Button>
+    </div>
+  </div>
 }
