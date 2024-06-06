0e9833faf45e9707b04ac3581271a8c78b225dbf
mohits-git <mohitkhatri2805@gmail.com>
2024-06-06 16:46:56 +0530

    feat(user-app): add generic Appbar in client app

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,6 +1,4 @@
 'use client'
-import { Appbar } from "@repo/ui/app-bar";
-import { signIn, signOut, useSession } from "next-auth/react";
 import React from "react";
 
 type Props = {
@@ -8,10 +6,9 @@ type Props = {
 }
 
 const Page: React.FC<Props> = () => {
-  const session = useSession();
   return (
     <div>
-      <Appbar user={session.data?.user} onSignin={signIn} onSignout={signOut} />
+      User Page
     </div>
   )
 }
