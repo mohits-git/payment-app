6e737314b864c0a72e18209ebc6a3d0b35ad3c29
mohits-git <mohitkhatri2805@gmail.com>
2024-06-06 16:49:51 +0530

    feat(merchant-app): add generic Appbar in client app

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -1,6 +1,3 @@
-'use client'
-import { Appbar } from "@repo/ui/app-bar";
-import { signIn, signOut, useSession } from "next-auth/react";
 import React from "react";
 
 type Props = {
@@ -8,10 +5,9 @@ type Props = {
 }
 
 const Page: React.FC<Props> = () => {
-  const session = useSession();
   return (
     <div>
-      <Appbar user={session.data?.user} onSignin={signIn} onSignout={signOut} />
+      Merchant app
     </div>
   )
 }
