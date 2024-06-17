e6ec2a3b5d264f5b0e7003b0dc0a3e7d5b6e14b0
mohits-git <mohitkhatri2805@gmail.com>
2024-06-17 10:23:56 +0530

    refac(user-app): refact root page.tsx to redirect user to login or dashboard based on the login status

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -1,16 +1,19 @@
-'use client'
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 import React from "react";
+import { authOptions } from "../lib/auth";
 
 type Props = {
 
 }
 
-const Page: React.FC<Props> = () => {
-  return (
-    <div>
-      User Page
-    </div>
-  )
+const Page: React.FC<Props> = async () => {
+  const session = await getServerSession(authOptions);
+  if (session?.user) {
+    redirect('/dashboard')
+  } else {
+    redirect('/api/auth/signin')
+  }
 }
 
-export default Page
+export default Page;
