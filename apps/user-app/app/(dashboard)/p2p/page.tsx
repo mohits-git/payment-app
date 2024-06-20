c818139f0149d84862618938eeaeb9a7aa656ecc
mohits-git <mohitkhatri2805@gmail.com>
2024-06-20 19:46:44 +0530

    feat(user-app): add send money card in p2p page

---
+++ apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SendMoneyCard } from "../../_components/send-money-card";
+
+const Page: React.FC = () => {
+  return (
+    <div className="w-full">
+      <SendMoneyCard />
+    </div>
+  )
+}
+
+export default Page
