a7e8634b3c23345705192fc2eff9cdb819199539
mohits-git <mohitkhatri2805@gmail.com>
2024-06-20 19:30:37 +0530

    feat(ui/icons): add P2PTransferIcon and navigate option in sidebar (user-app)

--- apps/user-app/app/_components/sidebar/index.tsx
+++ apps/user-app/app/_components/sidebar/index.tsx
@@ -3,6 +3,7 @@ import SidebarItem from "./sidebar-item";
 import { HomeIcon } from "@repo/ui/icons/home-icon";
 import { IconsTransferIcon } from "@repo/ui/icons/transfer-icon";
 import { IconsTransactionsIcon } from "@repo/ui/icons/transactions-icon";
+import { P2PTransferIcon } from "@repo/ui/icons/p2p-transfer-icon";
 
 const Sidebar: React.FC = () => {
   return (
@@ -11,6 +12,7 @@ const Sidebar: React.FC = () => {
         <SidebarItem href={"/dashboard"} icon={<HomeIcon />} title="Home" />
         <SidebarItem href={"/transfer"} icon={<IconsTransferIcon />} title="Transfer" />
         <SidebarItem href={"/transactions"} icon={<IconsTransactionsIcon />} title="Transactions" />
+        <SidebarItem href={"/p2p"} icon={<P2PTransferIcon />} title="P2P Transfer" />
       </div>
     </div>
   )
