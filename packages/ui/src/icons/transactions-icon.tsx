f8acf7813d554a8da36762e432acd71220220519
mohits-git <mohitkhatri2805@gmail.com>
2024-06-23 11:29:51 +0530

    fix(ui/icons): fix dom errors

--- packages/ui/src/icons/transactions-icon.tsx
+++ packages/ui/src/icons/transactions-icon.tsx
@@ -1,7 +1,7 @@
 export const IconsTransactionsIcon = () => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
     </svg>
   );
 };
