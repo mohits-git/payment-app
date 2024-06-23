41c7910afc656703876bf47957b91965cd64a341
mohits-git <mohitkhatri2805@gmail.com>
2024-06-23 11:27:47 +0530

    feat(user-app): add p2pTransactions to show all transactions

---
+++ apps/user-app/app/_components/p2p-transactions.tsx
@@ -0,0 +1,49 @@
+import { Card } from "@repo/ui/card"
+
+const P2PTransactions = ({
+  transactions
+}: {
+  transactions: {
+    timeStamp: Date,
+    amount: number,
+    fromUser?: string,
+    toUser?: string,
+  }[]
+}) => {
+  if (!transactions.length) {
+    return (
+      <Card title="Recent Transactions">
+        <div className="text-center pb-8 pt-8">
+          No Recent transactions
+        </div>
+      </Card>
+    )
+  }
+  return (
+    <Card title="Recent Transactions">
+      <div className="pt-2">
+        {transactions.map(t => <div className="flex justify-between">
+          <div>
+            <div className="text-sm">
+              {t.fromUser ? "Received " : "Sent "} INR
+            </div>
+            <div className="text-slate-600 text-xs">
+              {t.timeStamp.toDateString()}
+            </div>
+          </div>
+          <div className="flex flex-col justify-center">
+            <div>
+              {t.fromUser ? "+ " : "- "} Rs {t.amount / 100}
+            </div>
+            <div className="text-slate-600 text-xs">
+              {t.fromUser ? `From ${t.fromUser}` : `To ${t.toUser}`}
+            </div>
+          </div>
+
+        </div>)}
+      </div>
+    </Card>
+  )
+}
+
+export default P2PTransactions;
