aa9289d93dd881af0e44f46b9dd6abcef221b9c3
mohits-git <mohitkhatri2805@gmail.com>
2024-06-23 11:28:33 +0530

    refac(user-app): refac p2p page with transactions

--- apps/user-app/app/(dashboard)/p2p/page.tsx
+++ apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -1,10 +1,56 @@
 import React from "react";
 import { SendMoneyCard } from "../../_components/send-money-card";
+import P2PTransactions from "../../_components/p2p-transactions";
+import prisma from "@repo/db/client";
+import { getServerSession } from "next-auth";
 
-const Page: React.FC = () => {
+const Page: React.FC = async () => {
+  const session = await getServerSession();
+  const txn = await prisma.p2pTransfer.findMany({
+    where: {
+      // @ts-ignore
+      fromUserId: Number(session?.user?.id) || -1
+    },
+    include: {
+      toUser: true
+    }
+  });
+  const txnReceived = await prisma.p2pTransfer.findMany({
+    where: {
+      // @ts-ignore
+      toUserId: Number(session?.user?.id) || -1
+    },
+    include: {
+      fromUser: true
+    }
+  });
+
+
+  const transactions = txn.map(t => ({
+    amount: t.amount,
+    timeStamp: t.timestamp,
+    toUser: t.toUser.name!
+  }));
+  const transactionsRec = txnReceived.map(t => ({
+    amount: t.amount,
+    timeStamp: t.timestamp,
+    fromUser: t.fromUser.name!
+  }));
   return (
-    <div className="w-full">
-      <SendMoneyCard />
+    <div className="w-[calc(100vw-288px)]">
+      <div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold">
+        P2P Transfer
+      </div>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 p-4">
+        <div>
+          <SendMoneyCard />
+        </div>
+        <div>
+          <div className="pt-4">
+            <P2PTransactions transactions={[...transactions, ...transactionsRec]} />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
